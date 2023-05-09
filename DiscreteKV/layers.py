294e75adaa152dd09ab2964d3ceb0f324859b188
heyuwei <he20010515@163.com>
2023-05-09 19:12:33 +0800

    first commit

---
+++ DiscreteKV/layers.py
@@ -0,0 +1,13 @@
+'''
+Author: heyuwei he20010515@163.com
+Date: 2023-05-09 19:11:24
+LastEditors: heyuwei he20010515@163.com
+LastEditTime: 2023-05-09 19:11:42
+FilePath: /DiscreteKV/DiscreteKV/layers.py
+Description: layers
+'''
+import torch
+
+class CodeBook():
+    pass
+
